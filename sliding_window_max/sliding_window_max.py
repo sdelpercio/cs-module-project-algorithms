85e5de66e3e1bdd813fcc7502809e72bfb21b385
Shawn DelPercio <sdelperc@gmail.com>
2020-06-17 15:31:41 -0400

    implemented sliding window max initial

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,10 +3,23 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
-    # Your code here
+    # make start and end vars, new array for maxes
+    start = 0
+    end = k
+    max_nums = []
+    
+    # while end is less than length of nums - 1
+    while end <= len(nums):
+        # slice array
+        # get max from sliced array
+        max_num = max(nums[start:end])
+        # add max into new array
+        max_nums.append(max_num)
+        # increment start and end
+        start += 1
+        end += 1
 
-    pass
-
+    return max_nums
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
