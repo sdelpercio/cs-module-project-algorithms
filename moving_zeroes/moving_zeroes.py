4677b89e5ce6af7f7e16e6a57affb9f98b96643d
Shawn DelPercio <sdelperc@gmail.com>
2020-06-17 17:20:11 -0400

    implemented moving zeroes initial

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,13 +3,28 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
+    # if theres no zeroes, just return the list
+    if 0 not in arr:
+        return arr
+    
+    # otherwise
+    else:
+        i = 0
+        # loop through array
+        while i < len(arr):
+            # find next item that is non-zero, pop off, insert at beginning
+            if arr[i] != 0:
+                popped = arr.pop(i)
+                arr.insert(0, popped)
+                i += 1
+            else:
+                i += 1
+        
+        return arr
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
-    arr = [0, 3, 1, 0, -2]
+    arr = [0, 3, 1, 0, -2, 0]
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
