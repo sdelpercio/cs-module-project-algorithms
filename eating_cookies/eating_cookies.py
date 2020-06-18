93d5617706985366fe5fc16cdcfd1682a0b6a581
Shawn DelPercio <sdelperc@gmail.com>
2020-06-18 16:52:53 -0400

    improved eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -8,20 +8,22 @@ def eating_cookies(n, cache=None):
         return 0
     elif n == 1 or n == 0:
         return 1
-    # recursive cases without cache
-    if cache == None:
-        return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+    elif n == 2:
+        return 2
+    # if no cache, create one
+    if cache is None:
+        cache = [0 for i in range(n + 1)]
     # recursive cases with cache
-    else:
-        if cache[n]:
-            return cache[n]
-        else:
-            cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
-            
-            return cache[n]
+    
+    if cache[n]:
+        return cache[n]
+        
+    cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    
+    return cache[n]
         
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 12
+    num_cookies = 3
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
